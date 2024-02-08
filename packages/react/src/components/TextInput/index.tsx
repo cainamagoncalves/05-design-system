32cb966fb1e4eed78a2348d5113fdd8ae149ee63
Cainã Gonçalves <moaraadrean@gmail.com>
2024-02-08 16:05:08 -0300

    fix: Add ref to TextInput and adjust styles

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,17 +1,20 @@
 import { VariantProps } from '@stitches/react'
 import { Input, Prefix, TextInputContainer } from './styles'
+import { ElementRef, forwardRef } from 'react'
 
 export type TextInputProps = VariantProps<typeof Input> & {
   prefix?: string
 }
 
-export function TextInput({ prefix, ...props }: TextInputProps) {
-  return (
-    <TextInputContainer>
-      {!!prefix && <Prefix>{prefix}</Prefix>}
-      <Input {...props} />
-    </TextInputContainer>
-  )
-}
+export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(
+  ({ prefix, ...props }, ref) => {
+    return (
+      <TextInputContainer>
+        {!!prefix && <Prefix>{prefix}</Prefix>}
+        <Input {...props} ref={ref} />
+      </TextInputContainer>
+    )
+  },
+)
 
 TextInput.displayName = 'TextInput'
